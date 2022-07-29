8706f22b916d43c2c37685f159d9fff76b6dc7cc
Amine KASSI <Amine.K4595@gmail.com>
2022-07-29 15:11:05 +0200

    final commit

--- accueil.js
+++ accueil.js
@@ -1,1 +1,46 @@
-function afficherVendeur
+let afficherVendeur = () => {
+
+    const nom = document.getElementById('nom');
+    const categorie = document.getElementById('categorie');
+    const email = document.getElementById('email');
+    const tel = document.getElementById('tel');
+
+    const storageNom = sessionStorage.getItem('nom'); 
+    const storageCategorie = sessionStorage.getItem('categorie'); 
+    const storageEmail = sessionStorage.getItem('email'); 
+    const storageTel = sessionStorage.getItem('tel'); 
+    
+
+    nom.innerHTML = storageNom;
+    categorie.innerHTML = storageCategorie;
+    email.innerHTML = storageEmail;
+    tel.innerHTML = storageTel;
+}
+
+afficherVendeur();
+
+
+function Bien(nom, description, prix, image){
+    this.nom = nom;
+    this.description = description;
+    this.prix = prix;
+    this.image = image;
+}
+
+let Bien1 = new Bien('Hache d\'arme anti weeb', 'Une arme VERSATILE pour exterminer les weebs quelque soit la situation.', '50000 acier', 'https://pm1.narvii.com/6513/d1a9e083b284f0ec25ffc3f78da5c9058455442e_hq.jpg');
+
+let Bien2 = new Bien('Casse Tête', 'BONK!', '200 QI', "https://darksouls3-fr.wiki.fextralife.com/file/Dark-Souls-3/large_club.png");
+
+let afficherBien = () => {
+    const nom = document.getElementById('produit');
+    const description = document.getElementById('description');
+    const prix = document.getElementById('prix');
+    const image = document.getElementById('img');
+
+    nom.innerHTML = Bien1.nom;
+    description.innerHTML = Bien1.description;
+    prix.innerHTML = Bien1.prix;
+    image.src = Bien1.image;
+}
+
+afficherBien();
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-function Vendeur(nom, categorie, email, numeroTel)
+function Vendeur(nom, categorie, email, tel)
 {
     this.nom = nom;
     this.categorie = categorie;
     this.email = email;
-    this.numeroTel = numeroTel;
+    this.tel = tel;
 }
 
 
@@ -13,30 +13,38 @@ function ajouterVendeur()
     var nom = document.getElementById('nom').value;
     var categorie = document.getElementById('categorie').value;
     var email = document.getElementById('email').value;
-    var numeroTel = document.getElementById('numeroTel').value;
+    var tel = document.getElementById('tel').value;
 
-    var regex = /[a-zA-Z0-9_@\.\-]/;
+    var regexString = /[a-zA-Z0-9]/;
+    var regexEmail = /[a-zA-Z@]/;
+    var regexTel = /[0-9]/;
 
-    if(regex.test(nom) && !nom.match(/\S/))
+    if(regexString.test(nom) && nom.match(/\S/))
     {
         sessionStorage.setItem('nom', nom);
         sessionStorage.setItem('categorie', categorie);
-        sessionStorage.setItem('email', email);
+        
         alert("Ok");
     }else{
         alert ("Le champs est vide ou ne respecte pas les conditions demandées.");
     }
 
-    if(numeroTel.match(/[0-9]/) && !numeroTel.match(/\S/))
+    if(regexEmail.test(email) && email.match(/\S/))
     {
-        sessionStorage.setItem('numeroTel', numeroTel);
+        sessionStorage.setItem('email', email);
     }else{
-        alert ("Le champs est vide ou ne respecte pas les conditions demandées.");
+        alert ("Le champs du mail est vide ou ne respecte pas les conditions demandées.");
     }
-  
+
+    if(regexTel.test(tel) && tel.match(/\S/))
+    {
+        sessionStorage.setItem('tel', tel);
+    }else{
+        alert ("Le champs du téléphone est vide ou ne respecte pas les conditions demandées.");
+    }
 }
 
-ajouterVendeur();
+
 
 
 
