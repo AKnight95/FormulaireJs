40b9dd165809eded2aa44a104ecc95eef740468e
Amine KASSI <Amine.K4595@gmail.com>
2022-07-29 13:34:45 +0200

    test

--- index.js
+++ index.js
@@ -20,29 +20,18 @@ function ajouterVendeur()
     if(regex.test(nom) && !nom.match(/\S/))
     {
         sessionStorage.setItem('nom', nom);
-    }else{
-        alert ("Le champs est vide ou ne respecte pas les conditions demandées.")
-    }
-
-    if(regex.test(categorie) && !categorie.match(/\S/))
-    {
         sessionStorage.setItem('categorie', categorie);
-    }else{
-        alert ("Le champs est vide ou ne respecte pas les conditions demandées.")
-    }
-
-    if(regex.test(email) && !email.match(/\S/))
-    {
         sessionStorage.setItem('email', email);
+        alert("Ok");
     }else{
-        alert ("Le champs est vide ou ne respecte pas les conditions demandées.")
+        alert ("Le champs est vide ou ne respecte pas les conditions demandées.");
     }
 
     if(numeroTel.match(/[0-9]/) && !numeroTel.match(/\S/))
     {
         sessionStorage.setItem('numeroTel', numeroTel);
     }else{
-        alert ("Le champs est vide ou ne respecte pas les conditions demandées.")
+        alert ("Le champs est vide ou ne respecte pas les conditions demandées.");
     }
   
 }
